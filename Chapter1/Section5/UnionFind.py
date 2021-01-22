be22e82a038d4e69d6ea95aa99b27a091b858cdc
blestdata <blestdata@gmai.com>
2021-01-22 16:31:37 +0530

    Quick Union implementation

--- Chapter1/Section5/UnionFind.py
+++ Chapter1/Section5/UnionFind.py
@@ -24,11 +24,36 @@ class QuickFind(UF):
     def connected(self, v, w):
         return self.id[v] == self.id[w]
 
-    def union(self, v, w):
-        p = self.id[v]
-        q = self.id[w]
+    def union(self, p, q):
+        p = self.id[p]
+        q = self.id[q]
 
         if p != q:
             for i in range(len(self.id)):
                 if self.id[i] == p:
                     self.id[i] = q
+
+class QuickUnion(UF):
+    id = None
+
+    def __init__(self, capacity):
+        self.id = [i for i in range(capacity)]
+        self.count = capacity
+
+    def root(self, p):
+        while p != self.id[p]:
+            p = self.id[p]
+        return p
+
+    def connected(self, p, q):
+        return self.root(p) == self.root(q)
+
+    def union(self, p, q):
+        p = self.root(p)
+        q = self.root(q)
+
+        if p == q:
+            return
+        self.id[p] = q
+        self.count = self.count - 1
+        return
